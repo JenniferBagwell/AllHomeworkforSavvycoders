7ee5971d52b5612144e35f4d142a24c551125cce
JenniferBagwell <jdawbagwell@gmail.com>
2020-09-15 22:24:47 -0500

    h3

---
+++ homeworkweek3.js
@@ -0,0 +1,17 @@
+
+
+// Homework
+//Creat array of pizza toppings with 4 different toppings
+const pizzaToppings = [ "Kalua Pig", "Teri Sauce", "Green Onions", "Chicken Lau Lau"];
+
+//greetCustomer
+fucntion greetCustomer() {
+    console.log("Welcome to Pizza House, our toppings are:", pizzaToppings);}
+
+//Creat get pizza order with size crust and toppings
+const size = ["12in", "16in", "22in"];
+const crust = ["thin", "pan", "handtossed"];
+
+console.log('One', ${size[0]});
+
+
